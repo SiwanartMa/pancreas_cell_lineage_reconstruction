02e340394489e72946a92afe8bc0cd3c95c0cc1a
SiwanartMa <ycma0224@gmail.com>
2025-05-21 16:02:21 +0100

    Change marker genes, annotate cell types

--- readmes/trajectory_45_epith.R
+++ readmes/trajectory_45_epith.R
@@ -48,43 +48,34 @@ epith_4_5 <- FindClusters(epith_4_5, resolution = 0.7)
 
 # The markers from paper
 markers <- list(
-  dorsal_MP = c("NR2F1"),
-  ventral_MP = c("TBX3"),
-  PB_progenitors = c("ISL1", "SULT1E1", "HHEX", "NKX6-2"),
-  EHBD = c("SPP1"),
+  dorsal_MP = c("PDX1", "NR2F1"),
+  ventral_MP = c("PDX1","TBX3"),
+  PB_progenitors = c("ISL1", "SULT1E1"),
+  EHBD = c("SPP1", "SULT1E1"),
   enterocyte = c("CDX2"),
   hepatoblast = c("ALB")
 )
-
-DimPlot(epith_4_5, reduction = "umap", label = TRUE) + NoLegend()
-FeaturePlot(epith_4_5, unlist(markers), label = TRUE)
 
 epith_4_5 <- AddModuleScore(epith_4_5,
                             features = markers,
                             name = "Score")
 
-epith_4_5[[c("dMP_Score", "vMP_Score", "PB_Score", "EHBD_Score", "E_Score", "H_Score")]] <- NULL
 epith_4_5[[]]
 
 epith_4_5@meta.data <- epith_4_5@meta.data %>%  
-  dplyr::rename(dMP_Score = Score1,
-                vMP_Score = Score2,
+  dplyr::rename(dorsalMP_Score = Score1,
+                ventralMP_Score = Score2,
                 PB_Score = Score3,
                 EHBD_Score = Score4,
-                E_Score = Score5,
-                H_Score = Score6)
+                Enterocyte_Score = Score5,
+                Hepatoblast_Score = Score6)
 
-FeaturePlot(epith_4_5, c("PB_Score", "dMP_Score", "vMP_Score", "EHBD_Score", "E_Score", "H_Score"), label = TRUE, 
+FeaturePlot(epith_4_5, c("PB_Score", "dorsalMP_Score", "ventralMP_Score", "EHBD_Score", "Enterocyte_Score", "Hepatoblast_Score"), label = TRUE, 
             min.cutoff = 0, reduction = "umap")
-ggsave("~/Desktop/researchProject/integration/outputs/QC_plot/4_6PCW_reQC/epith_4_5_feature.png")
-
-VlnPlot(epith_4_5, features = "dMP_Score", pt.size = 0) +
-  stat_summary(fun.y = median, geom='point', size = 2, colour = "black") +
-  geom_hline(yintercept=0, linetype="dashed", color = "red") & NoLegend()
-
+#ggsave("~/Desktop/researchProject/integration/outputs/QC_plot/4_6PCW_reQC/epith_4_5_feature.png")
 
 # List of all scores to plot
-score_list <- c("dMP_Score", "vMP_Score", "PB_Score", "EHBD_Score", "E_Score", "H_Score")
+score_list <- c("dorsalMP_Score", "ventralMP_Score", "PB_Score", "EHBD_Score", "Enterocyte_Score", "Hepatoblast_Score")
 
 # Generate individual violin plots with median dot and reference line
 violin_plots <- lapply(score_list, function(score) {
@@ -102,6 +93,17 @@ combined_plot <- wrap_plots(violin_plots, ncol = 2)
 # Display the plot
 print(combined_plot)
 
+
+# Annotate clusters
+new.cluster.ids <- c("Enterocyte", "dorsalMP", "ventralMP", "Enterocyte", "PB + EHBD",
+                     "EHBD", "EHBD", "ventralMP", "Enterocyte", "Enterocyte")
+
+names(new.cluster.ids) <- levels(epith_4_5)
+epith_4_5 <- RenameIdents(epith_4_5, new.cluster.ids)
+DimPlot(epith_4_5, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
+#ggsave("~/Desktop/researchProject/integration/outputs/QC_plot/4_6PCW_reQC/epith_4_5_celltype.png")
+
+# Pseudotime
 data <- as.matrix(t(epith_4_5@assays$SCT@data))
 data <- data[, VariableFeatures(epith_4_5)]
 dm <- DiffusionMap(data, n_pcs = 3)
@@ -118,18 +120,17 @@ slingPseudotime(sds)[,1]
 
 epith_4_5$pseudotime <- slingPseudotime(sds)[,1]
 
-FeaturePlot(epith_4_5, "pseudotime", reduction = "umap", label = T) +
+FeaturePlot(epith_4_5, "pseudotime", reduction = "umap", label = T,) +
   scale_color_gradient(low = "lightgreen", high = "orange")
+#ggsave("~/Desktop/researchProject/integration/outputs/QC_plot/4_6PCW_reQC/epith_4_5_pseudotime.png")
 
-FeatureScatter(epith_4_5, "pseudotime", "NR2F1") + geom_smooth()
+FeatureScatter(epith_4_5, "pseudotime", "ISL1") + 
+  geom_smooth() +
+  ggtitle("ISL1 Expression vs. Pseudotime")
 
-FeatureScatter(epith_4_5, "pseudotime", "ISL1") + geom_smooth()
-
-FeatureScatter(epith_4_5, "pseudotime", "HHEX") + geom_smooth()
-
-FeatureScatter(epith_4_5, "pseudotime", "TBX3") + geom_smooth()
-
-FeatureScatter(epith_4_5, "pseudotime", "SPP1") + geom_smooth()
+FeatureScatter(epith_4_5, "pseudotime", "SULT1E1") + 
+  geom_smooth()+
+  ggtitle("SULT1E1 Expression vs. Pseudotime")
 
 # Save processed object
 saveRDS(epith_4_5, file = "~/Desktop/researchProject/integration/outputs/QC_seurat/epithelial_PWC4_5_harmony.rds")
